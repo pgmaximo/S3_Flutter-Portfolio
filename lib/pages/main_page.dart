727ea33d3f9e28b9f4eae52997b31421c2c20178
pgmaximo <pedrogmaximo@gmail.com>
2024-05-06 09:26:01 -0300

    Adição das novas classes sobre as seções da pagina

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-String gitURL = 'https://github.com/pgmaximo';
-String linURL = 'https://www.linkedin.com/in/pedro-gabriel-maximo/';
+import 'package:portfolio/widgets/appbar.dart';
+import 'package:portfolio/widgets/section/home.dart';
+import 'package:portfolio/widgets/section/projetos.dart';
+import 'package:portfolio/widgets/section/sobremim.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -15,54 +14,16 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'PedroMáximo.dev',
-          textAlign: TextAlign.left,
-          style: TextStyle(fontWeight: FontWeight.bold, fontStyle: FontStyle.italic, fontSize: 20),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () async {        
-              if (await canLaunchUrl(gitURL as Uri)) {
-                await launchUrl(gitURL as Uri);
-              } else {
-                print('Could not launch URL: $gitURL');
-              }
-            },
-            icon: const FaIcon(FontAwesomeIcons.github),
-            tooltip: 'Github',
-          ),
-          IconButton(
-            onPressed: () {
-
-            },
-            icon: const FaIcon(FontAwesomeIcons.linkedin),
-            tooltip: 'LinkedIn',
-          ),
-        ]
-      ),
+    return const Scaffold(
+      // appBar: MyAppBar(),
 
       body: SingleChildScrollView(
         child: Column(
           children: [
-            //Menu
-            Container(
-              
-            ),
-            //Sobre mim
-            Container(
-              
-            ),
-            //Projetos
-            Container(
-              
-            ),
-            //Contatos
-            Container(
-              
-            ),
+            HomeMenu(),
+            SobreMim(),
+            Projetos(),
+            // Contato(),
           ],
         ),
       )
--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -1,56 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+ 
+String gitURL = 'https://github.com/pgmaximo';
+String linURL = 'https://www.linkedin.com/in/pedro-gabriel-maximo/';
 
 class MyAppBar extends StatelessWidget {
-  final GlobalKey menuId;
-  final GlobalKey sobremimId;
-  final GlobalKey projetosId;
-  final GlobalKey contatosId;
-
-  const MyAppBar({
-    super.key,
-    required this.menuId,
-    required this.sobremimId,
-    required this.projetosId,
-    required this.contatosId,
-  });
-
-  void scrollToSection(GlobalKey key) {
-    Scrollable.ensureVisible(
-      key.currentContext!,
-      duration: const Duration(seconds: 2),
-      curve: Curves.linear,
-    );
-  }
+  const MyAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: const Text(
+      title: const Text( 
         'PedroMáximo.dev',
         textAlign: TextAlign.left,
-        style: TextStyle(fontWeight: FontWeight.bold, fontStyle: FontStyle.italic, fontSize: 20),
+        style: TextStyle(
+          fontWeight: FontWeight.bold, 
+          fontStyle: FontStyle.italic, 
+          fontSize: 20, 
+          color: Color.fromARGB(1, 0, 26, 118)
+        ),
       ),
       actions: [
-        GestureDetector(
-          onTap: () => scrollToSection(menuId),
-          child: const Text('Menu'),
+        IconButton(
+          // Rota para o site externo Github
+          onPressed: () async {        
+            if (await canLaunchUrl(Uri.parse(gitURL))) {
+              await launchUrl(Uri.parse(gitURL));
+            }
+          },
+          icon: const FaIcon(FontAwesomeIcons.github),
+          tooltip: 'Github',
         ),
-        const SizedBox(width: 20),
-        GestureDetector(
-          onTap: () => scrollToSection(sobremimId),
-          child: const Text('Sobre mim'),
+        IconButton(
+          // Rota para o site externo LinkedIn
+          onPressed: () async {        
+            if (await canLaunchUrl(Uri.parse(linURL))) {
+              await launchUrl(Uri.parse(linURL));
+            }
+          },
+          icon: const FaIcon(FontAwesomeIcons.linkedin),
+          tooltip: 'LinkedIn',
         ),
-        const SizedBox(width: 20),
-        GestureDetector(
-          onTap: () => scrollToSection(projetosId),
-          child: const Text('Projetos'),
-        ),
-        const SizedBox(width: 20),
-        GestureDetector(
-          onTap: () => scrollToSection(contatosId),
-          child: const Text('Contatos'),
-        ),
-      ],
+      ]
     );
   }
 }
